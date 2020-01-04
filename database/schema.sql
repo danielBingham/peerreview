6b58bcbdc86814b3cee561e28efad5a4574dfb8c
Daniel Bingham <dbingham@theroadgoeson.com>
2020-01-04 17:24:02 -0500

    Issue #2 - Creating the initial database structure.

--- database/schema.sql
+++ database/schema.sql
@@ -1,26 +1,29 @@
 /* Peer Review Schema file */
 
-CREATE TABLE `recipes` (
+CREATE TABLE `users` (
     `id`    BIGINT unsigned NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(512),
-    `source_url`    VARCHAR(512),
-    `created_date`  DATETIME,
-    `update_date`   DATETIME,
-    PRIMARY KEY(`id`),
-    INDEX `title` (`title`)
-);
-
-CREATE TABLE `ingredients` (
-    `id`    BIGINT unsigned NOT NULL AUTO_INCREMENT,
-    `name`  VARCHAR(512),
-    PRIMARY KEY(`id`),
+    `name`  VARCHAR(256),
+    `password`  VARCHAR(64),
+    `email`     VARCHAR(256),
+    PRIMARY KEY (`id`),
     INDEX `name` (`name`)
 );
 
-CREATE TABLE `recipe_ingredients` (
-    `recipe_id`     BIGINT unsigned NOT NULL,
-    `ingredient_id` BIGINT unsigned NOT NULL,
-    PRIMARY KEY(`recipe_id`,`ingredient_id`)
+CREATE TABLE `papers` (
+    `id`    BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR (512),
+    `content`   TEXT,
+    `url`       VARCHAR(512),
+    PRIMARY KEY (`id`),
+    INDEX `title` (`title`),
+    INDEX `content` (`content`)
+);
+
+CREATE TABLE `paper_authors` (
+    `user_id`   BIGINT unsigned NOT NULL,
+    `paper_id`  BIGINT unsigned NOT NULL,
+    `author_order`     INT unsigned NOT NULL,
+    PRIMARY KEY (`user_id`,`paper_id`) 
 );
     
 
