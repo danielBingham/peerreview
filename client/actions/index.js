3ef5d4a6b7e5ada888c4e8af72f6e15850659dd0
Daniel Bingham <dbingham@theroadgoeson.com>
2020-02-19 17:14:50 -0500

    Issue #3 - Initial actions and reducers.

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,38 +1,63 @@
-export const CREATE_USER = 'CREATE_USER';
-export const POST_USER = 'POST_USER';
-export const POST_USER_PASSWORD = 'POST_USER_PASSWORD';
-export const REGISTER_USER = 'REGISTER_USER';
 
 const backend = '';
 
-export const createUser = user => ({
-    type: CREATE_USER,
-    user: user
-});
-
-export const postUser = user => dispatch => {
-    return fetch(backend + '/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => response.json())
-    .then(user => dispatch(createUser(user)));
+/**
+ * An action and action creator to revieve a user from the backend and update
+ * the store.
+ */
+export const RECIEVE_USER = 'RECIEVE_USER';
+export const recieveUser = function(user) { 
+    return {
+        type: RECIEVE_USER,
+        user: user
+    };
 };
 
-export const postUserPassword = user => dispatch => {
-    return fetch(backend + '/' + user.id + '/password', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({password: user.password});
-    });
+
+/**
+ * A thunk action creator to post a user to the backend and retrieve the
+ * complete user (with ID) in response.  It then calls recieveUser() to add the
+ * user to our store.
+ */
+export const postUser = function(user) { 
+    return function(dispatch) {
+        return fetch(backend + '/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(response => response.json())
+        .then(user => dispatch(recieveUser(user)));
+    };
 };
 
-export const registerUser = user => dispatch => {
-    dispatch(postUser(user));
-    return dispatch(postUserPassword(user));
+/**
+ * We don't want to store the user's password on the front end, so this method
+ * simply updates the user's password on the backend.  It doesn't update state
+ * at all and that's intentional.
+ */
+export const postUserPassword = function(user) { 
+    return function(dispatch) {
+        return fetch(backend + '/' + user.id + '/password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({password: user.password});
+        });
+    };
+};
+
+/**
+ * A thunk action creator to execute the complete user registration flow:
+ * - post the user to the backend (updating the store with the new user)
+ * - post the user's password to the backend (don't update the store)
+ */
+export const registerUser = function(user) { 
+    return function(dispatch) {
+        dispatch(postUser(user));
+        return dispatch(postUserPassword(user));
+    };
 };
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,23 @@
 import { combineReducers } from 'redux';
 import {
-    CREATE_USER, POST_USER,
+    RECIEVE_USER, POST_USER,
     POST_USER_PASSWORD, REGISTER_USER } from '../actions';
 
+const initialState = {
+    registeringUser: {}, 
+    users: {}
+};
 
-const rootReducer = combineReducers({
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case RECIEVE_USER:
+            return Object.assign({}, state, {
+                users: Object.assign({}, state.users, { [action.user.id]: action.user })
+            });
+        default:
+            return state;
+    }
+}
 
-});
 
 export default rootReducer;
