21379ecefbb9248015231fcd4fb7cc7fed1e0e28
Daniel Bingham <dbingham@theroadgoeson.com>
2022-07-29 20:18:02 -0400

    Issue #40 - Staging is partially working.  Need to get spaces working.

--- infrastructure/terraform/staging/domains/variables.tf
+++ infrastructure/terraform/staging/domains/variables.tf
@@ -1,3 +1,13 @@
 variable "app_load_balancer_ip" {
   description = "Ip address of the app's load balancer."
 }
+
+variable "region" {
+  description="The Digital Ocean region to launch into as our primary region."
+  default="nyc3"
+}
+
+variable "token" {
+  description = "Access token for Digital Ocean API."
+}
+
