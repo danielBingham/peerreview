3f59ca658666c2806bb8296667e7c80b03589ba5
Daniel Bingham <dbingham@theroadgoeson.com>
2022-08-04 13:37:41 -0400

    Removing the fields explanation, we'll handle this a different way.

--- client/pages/fields/FieldsListPage.js
+++ client/pages/fields/FieldsListPage.js
@@ -8,25 +8,6 @@ const FieldsListPage = function(props) {
 
     return (
         <div id="fields-list-page" className="page">
-            <div className="explanation">
-                <p>
-                    These are the top-level academic fields on the site.  Each
-                    field can have children, which can have children, and so on,
-                    forming the full heirarchy of academic disciplines.  
-                </p>
-                <p>
-                    Defining a
-                    new top-level field takes the support of a significant
-                    percentage of the community.  Defining a new child field takes
-                    the support of peers with a certain reputation in its parent.  
-                </p>
-
-                <p>
-                    To view the children of a particular field, as well as the
-                    papers published in that field and its children, click into
-                    it.
-                </p>
-            </div>
             <FieldListView />
         </div>
     )
