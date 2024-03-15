27bf86a27f9c2c8c8b34819f0772bc8e641c542b
Daniel Bingham <dbingham@theroadgoeson.com>
2024-03-15 11:23:47 -0400

    Removing previously committed `dist` directory.

--- packages/model/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FileFixtures = exports.PaperFixtures = void 0;
-const Paper_1 = require("./fixtures/Paper");
-Object.defineProperty(exports, "PaperFixtures", { enumerable: true, get: function () { return Paper_1.results; } });
-const File_1 = require("./fixtures/File");
-Object.defineProperty(exports, "FileFixtures", { enumerable: true, get: function () { return File_1.results; } });
--- packages/model/dist/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { Paper, PaperAuthor, PaperVersion } = require('./models/Paper');
-exports.Paper = Paper;
-exports.PaperAuthor = PaperAuthor;
-exports.PaperVersion = PaperVersion;
-const { File } = require('./models/File');
-exports.File = File;
-exports.PaperFixtures = require('./fixtures/Paper');
-exports.FileFixtures = require('./fixtures/File');
