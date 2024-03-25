ab68b2cc3ed27276fe8a0758f32098dbb2007d2d
Daniel Bingham <dbingham@theroadgoeson.com>
2024-03-25 18:57:52 -0400

    Updating FilesDAO with getFilesTable fixture.  Coulda sworn I had already done this.

--- packages/backend/test/daos/FilesDAO.spec.ts
+++ packages/backend/test/daos/FilesDAO.spec.ts
@@ -3,7 +3,7 @@ import { QueryResult } from 'pg'
 
 import FilesDAO from '../../src/daos/FileDAO'
 
-import { result } from '../fixtures/database/FilesTable'
+import { getFilesTableFixture } from '../fixtures/database/FilesTable'
 import { File, getFileFixture, ResultType, DatabaseResult } from '@danielbingham/peerreview-model'
 
 import { mockCore } from '../mocks/MockCore'
@@ -18,7 +18,10 @@ describe('FilesDAO', function() {
     describe('hydrateFile()', function() {
         it('should properly hydrate a single File based on a single QueryResultRow', async function() {
             const fileDAO = new FilesDAO(mockCore)
-            const hydratedResult = fileDAO.hydrateFile(result.rows[0])
+
+
+            const tableFixture = getFilesTableFixture()
+            const hydratedResult = fileDAO.hydrateFile(tableFixture.rows[0])
 
             const fixture = getFileFixture(ResultType.Database) as DatabaseResult<File>
             expect(hydratedResult).toEqual(fixture.dictionary[1])
@@ -28,7 +31,9 @@ describe('FilesDAO', function() {
     describe('hydrateFiles()', function() {
         it('should properly interpret a QueryResultRow[] and return DatabaseResult<File>', async function() {
             const fileDAO = new FilesDAO(mockCore)
-            const hydratedResults = fileDAO.hydrateFiles(result.rows)
+
+            const tableFixture = getFilesTableFixture()
+            const hydratedResults = fileDAO.hydrateFiles(tableFixture.rows)
 
             const fixture = getFileFixture(ResultType.Database) as DatabaseResult<File>
             expect(hydratedResults).toEqual(fixture)
@@ -37,11 +42,13 @@ describe('FilesDAO', function() {
 
     describe('selectFiles()', function() {
         it('should return a properly populated result set', async function() {
+            const tableFixture = getFilesTableFixture()
             mockCore.database.query.mockImplementation(function() {
-                return new Promise<QueryResult>(resolve => resolve(result))
+                return new Promise<QueryResult>(resolve => resolve(tableFixture))
             })
 
             const fileDAO = new FilesDAO(mockCore)
+
             const results = await fileDAO.selectFiles()
 
             const fixture = getFileFixture(ResultType.Database) as DatabaseResult<File>
